3f48c445d2e702135a5ebfb37dfdb88788de1015
Alexandre DuBreuil <code@alexandredubreuil.com>
2019-07-25 20:09:52 +0200

    wip cleanup

--- code/ch03/client.py
+++
@@ -1,8 +0,0 @@
-from multiprocessing import Process, Pipe
-
-def f(child_conn):
-    p = Process(target=f, args=(child_conn,))
-    p.start()
-    msg = "Hello"
-    child_conn.send(msg)
-    child_conn.close()
--- code/ch03/daemon.py
+++
@@ -1,7 +0,0 @@
-import daemon
-
-# from drums_rnn import generate
-
-with daemon.DaemonContext():
-    # do_main_program()
-    pass
