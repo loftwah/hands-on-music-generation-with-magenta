3ea462d6087c63d68b575abd0977dbaa109decf0
Alexandre DuBreuil <code@alexandredubreuil.com>
2019-07-25 20:12:24 +0200

    wip move common to folder

--- code/common/wav2signal.py
+++ code/common/wav2signal.py
@@ -1,5 +1,4 @@
 import wave
-import math
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,7 +9,6 @@ with wave.open(file, 'r') as wav_file:
     # Extract Raw Audio from Wav File
     signal = wav_file.readframes(-1)
     signal = np.fromstring(signal, 'Int16')
-    signal = signal[math.floor(len(signal) * 0.251):math.floor(len(signal) * 0.2511)]
 
     # Split the data into channels
     channels = [[] for channel in range(wav_file.getnchannels())]
@@ -19,10 +17,10 @@ with wave.open(file, 'r') as wav_file:
 
     # Get time from indices
     fs = wav_file.getframerate()
-    Time = np.linspace(0, len(signal) / len(channels) / fs, num=len(signal) / len(channels))
+    time = np.linspace(0, len(signal) / len(channels) / fs, num=len(signal) / len(channels))
 
     # Plot
-    plt.figure(num=None, figsize=(3, 3), dpi=80, facecolor='w', edgecolor='k')
+    plt.figure(num=None, figsize=(16, 6), dpi=80, facecolor='w', edgecolor='k')
     for channel in channels:
-        plt.plot(Time, channel)
+        plt.plot(time, channel)
     plt.show()
